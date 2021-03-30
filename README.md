153d02adf14b43d4b2d26e69cb5f7008e22b92e0
seu-wll <923161273@qq.com>
2021-03-30 23:25:59 +0800

    2021/3/30

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # AutoScout
-Scouts  focus on MEME at media website
+预计开发时间：4.1-5.1
+
+网站：bilibili，douyin
+
+要求：
+
+1. 实现B站和抖音的相关内容解析，包括热点tag，视频形式，热点分区，获得bilibili与抖音相关的用户群像，为AutoBlogger选择领域做数据支撑。
+2. 监测b站与抖音以及AutoBlogger下属的各种账号，并返回相关数据以供调参。
+3. 爬取相关视频用于AutoBlogger原素材。
+4. 用网站的方式进行数据可视化呈现。
+
+
+
---
+++ b_download_user.py
@@ -0,0 +1,35 @@
+import requests
+
+def get_star(url):
+    html = requests.get(url).json()['data']
+    follower = html['follower']
+    following = html['following']
+    print("粉丝数：{}，关注数：{}".format(follower,following))
+    print("-"*20)
+
+def download_face(face_url,name):
+    image = requests.get(face_url).content
+    with open("./{}.jpg".format(name),'wb') as f:
+        f.write(image)
+
+def get_userdata(url):
+    jsondata = requests.get(url).json()['data']
+    name = jsondata['name']
+    sex = jsondata['sex']
+    level = jsondata['level']
+    birthday = jsondata['birthday']
+    coins = jsondata['coins']
+    print("名字是：{}  性别是：{}  等级是：{}  生日是：{}  硬币数目：{}".format(name,sex,level,birthday,coins))
+    face_url = jsondata['face']
+    print("正在下载头像……")
+    #download_face(face_url,name)
+
+def main():
+    for i in range(10000, 10010):
+        url = "https://api.bilibili.com/x/space/acc/info?mid={}&jsonp=jsonp".format(i)
+        get_userdata(url)
+        url2 = "https://api.bilibili.com/x/relation/stat?vmid={}&jsonp=jsonp".format(i)
+        get_star(url2)
+
+if __name__ == '__main__':
+    main()
